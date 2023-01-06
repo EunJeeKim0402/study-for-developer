b4c411e949f25a1c3538ab08215399d457b10f2d
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-01-06 20:40:40 +0900

    Create 01.dart

---
+++ dart/01.dart
@@ -0,0 +1,13 @@
+void main() {
+  // dart에서는 타입이 지정되지 않은 dynamic이라는 형식을 사용할 수 있음.
+  // dynamic은 꼭 필요할 때만 사용해야함.
+  //var name; //dynamic
+  dynamic name;
+  if(name is String){
+    name.
+  }
+  if(name is int){
+    name.
+  }
+ 
+}
