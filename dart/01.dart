f201595ed0b70a06fbb8875f5d31fa809c049dd0
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-01-09 22:13:56 +0900

    Update 01.dart

--- dart/01.dart
+++ dart/01.dart
@@ -241,7 +241,23 @@ void main(){
 }
 
 
-
+// Typedef
+// 자료형이 헷갈릴 때 도움이 될 alias를 만드는 방법
+/*
+typedef ListOfInts = List<int>;
+List<int> reverseListOfNumbers(List<int> list){
+  var reversed = list.reversed;
+  return reversed.toList();
+}
+*/
+typedef UserInfo = Map<String, String>;
+String sayHi(Map<String, String> userInfo){
+  return 'Hi ${userInfo['name']}';
+}
+void main(){
+  //print(reverseListOfNumbers([1,2,3]));
+  sayHi({'name': 'nico'});
+}
 
 
 
