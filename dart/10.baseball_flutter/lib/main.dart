0ebaa78e0be5e47c5983c0404f8f64c8852ec19d
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-05-25 19:54:25 +0900

    Update main.dart

--- dart/10.baseball_flutter/lib/main.dart
+++ dart/10.baseball_flutter/lib/main.dart
@@ -5,19 +5,16 @@ void main() {
   runApp(BaseballGame());
 }
 
-// StatefulWidget : 동적인 UI때 주로 사용. 화면이 다시그려지는 등...
-// StatelessWidget : 정적인 UI때 주로 사용. 한번 그려지면 변하지않음.
 class BaseballGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '숫자야규게임',
+      title: '숫자야구게임',
       home: GameScreen(),
     );
   }
 }
 
-// createState() 메소드가 호출될 때 _GameScreenState클래스의 인스턴스 생성&반환
 class GameScreen extends StatefulWidget {
   @override
   _GameScreenState createState() => _GameScreenState();
@@ -26,23 +23,27 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   Random random = Random();
   List<int> answerNumbers = [];
-  String input = '';
-  String message = '';
+  String input = ''; // 사용자의 입력을 저장
+  String message = ''; // 상태 메시지 저장
+  bool showAnswer = false; // 정답 보여줄지 말지
 
+  // 위젯이 초기화될때 호출됨. 초기화와 동시에 정답숫자 생성
   @override
   void initState() {
     super.initState();
     createAnswerNumbers();
   }
 
+  // 답변을 만드는 함수
   void createAnswerNumbers() {
-    while (answerNumbers.length < 3) {
-      int number = random.nextInt(9) + 1; // 1~9
-      // 중복제거
-      if (!answerNumbers.contains(number)) {
-        answerNumbers.add(number);
-      }
+    answerNumbers.clear(); // 리스트를 비움
+    Set<int> uniqueNumbers = Set<int>(); // Set은 중복값 불허
+
+    while (uniqueNumbers.length < 3) {
+      int number = random.nextInt(10); // 0~9
+      uniqueNumbers.add(number);
     }
+    answerNumbers = uniqueNumbers.toList();
   }
 
   void checkAnswer() {
@@ -53,8 +54,16 @@ class _GameScreenState extends State<GameScreen> {
       return;
     }
 
-    // int.parse로 문자 -> 정수. e는 매개변수? 임시변수
+    // 숫자를 문자열에서 분리해서 정수리스트 userNumbers에 저장
     List<int> userNumbers = input.split('').map((e) => int.parse(e)).toList();
+
+    if (duplicateNumbers(userNumbers)) {
+      setState(() {
+        message = '숫자가 중복되었습니다. 다시 입력해주세요.';
+        input = '';
+      });
+      return;
+    }
 
     int success = 0;
     int fail = 0;
@@ -81,12 +90,35 @@ class _GameScreenState extends State<GameScreen> {
     });
   }
 
+  // 중복확인
+  bool duplicateNumbers(List<int> numbers) {
+    Set<int> uniqueNumbers = Set<int>();
+
+    for (int number in numbers) {
+      if (uniqueNumbers.contains(number)) {
+        // 리스트 숫자 중 이미 uniqueNumbers에 존재하는 숫자면 중복이니깐 true
+        return true;
+      }
+      uniqueNumbers.add(number);
+    }
+    return false;
+  }
+
+  // 겜 초기화
   void resetGame() {
     setState(() {
       answerNumbers.clear();
       input = '';
       message = '';
       createAnswerNumbers();
+      showAnswer = false;
+    });
+  }
+
+  // 정답 보여주기
+  void showAnswerNumbers() {
+    setState(() {
+      showAnswer = true;
     });
   }
 
@@ -97,7 +129,7 @@ class _GameScreenState extends State<GameScreen> {
         title: Text('숫자 야구 게임'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0), // 위젯의 여백 - 네방향 패딩값 지정.
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
             Text(
@@ -105,10 +137,14 @@ class _GameScreenState extends State<GameScreen> {
               style: TextStyle(fontSize: 18.0),
             ),
             SizedBox(height: 16.0),
-            TextField(
+            TextField( // TextField를 이용해서 사용자의 입력을 받음. 입력 변경될때마다 onChanged
               onChanged: (value) {
                 setState(() {
-                input = value;
+                  if (value.length == 1 && input.contains(value)) {
+                    input = input.substring(0, input.length - 1);
+                  } else {
+                    input = value;
+                  }
                 });
               },
               keyboardType: TextInputType.number,
@@ -123,15 +159,25 @@ class _GameScreenState extends State<GameScreen> {
               child: Text('확인'),
             ),
             SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: resetGame,
+              child: Text('재시작'),
+            ),
+            SizedBox(height: 16.0),
+            if (showAnswer)
+              Text(
+                '정답: ${answerNumbers.join()}',
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ElevatedButton(
+              onPressed: showAnswerNumbers,
+              child: Text('정답 보기'),
+            ),
+            SizedBox(height: 16.0),
             Text(
               message,
               style: TextStyle(fontSize: 18.0),
               textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: resetGame,
-              child: Text('재시작'),
             ),
           ],
         ),
