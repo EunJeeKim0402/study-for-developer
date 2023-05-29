bdc67d88981edeb29b5559e674299903f9cf1ba9
Kim EunJee <103404604+EunJeeKim0402@users.noreply.github.com>
2023-05-29 19:50:47 +0900

    Update main.dart

--- dart/10.baseball_flutter/lib/main.dart
+++ dart/10.baseball_flutter/lib/main.dart
@@ -26,47 +26,45 @@ class _GameScreenState extends State<GameScreen> {
   String input = ''; // 사용자의 입력을 저장
   String message = ''; // 상태 메시지 저장
   bool showAnswer = false; // 정답 보여줄지 말지
+  bool trueInput = true; // 숫자인지 아닌지 유효성검사
 
-  // 위젯이 초기화될때 호출됨. 초기화와 동시에 정답숫자 생성
   @override
   void initState() {
     super.initState();
-    createAnswerNumbers();
+    createAnswerNumbers(); // 위젯 초기화 시 정답 숫자 생성
   }
 
-  // 답변을 만드는 함수
   void createAnswerNumbers() {
-    answerNumbers.clear(); // 리스트를 비움
+    answerNumbers.clear(); // 리스트 비우기
     Set<int> uniqueNumbers = Set<int>(); // Set은 중복값 불허
 
     while (uniqueNumbers.length < 3) {
-      int number = random.nextInt(10); // 0~9
+      int number = random.nextInt(10); // 0~9 사이의 난수 생성
       uniqueNumbers.add(number);
     }
-    answerNumbers = uniqueNumbers.toList();
+    answerNumbers = uniqueNumbers.toList(); // 리스트로 변환하여 저장
   }
 
   void checkAnswer() {
     if (input.length != 3) {
       setState(() {
-        message = '3개의 숫자를 입력해주세요.';
+        message = '3개의 숫자를 입력해주세요.'; // 입력이 3개가 아닐 경우 에러 메시지 설정
       });
       return;
     }
 
-    // 숫자를 문자열에서 분리해서 정수리스트 userNumbers에 저장
-    List<int> userNumbers = input.split('').map((e) => int.parse(e)).toList();
+    List<int> userNumbers = input.split('').map((e) => int.parse(e)).toList(); // 문자열에서 숫자 분리하여 리스트로 저장
 
     if (duplicateNumbers(userNumbers)) {
       setState(() {
-        message = '숫자가 중복되었습니다. 다시 입력해주세요.';
+        message = '숫자가 중복되었습니다. 다시 입력해주세요.'; // 중복된 숫자일 경우 에러 메시지 설정
         input = '';
       });
       return;
     }
 
-    int success = 0;
-    int fail = 0;
+    int success = 0; // 정답과 일치하는 숫자와 위치의 개수
+    int fail = 0; // 정답과 일치하지 않는 숫자의 개수
 
     for (int i = 0; i < userNumbers.length; i++) {
       if (userNumbers[i] == answerNumbers[i]) {
@@ -90,21 +88,18 @@ class _GameScreenState extends State<GameScreen> {
     });
   }
 
-  // 중복확인
   bool duplicateNumbers(List<int> numbers) {
     Set<int> uniqueNumbers = Set<int>();
 
     for (int number in numbers) {
       if (uniqueNumbers.contains(number)) {
-        // 리스트 숫자 중 이미 uniqueNumbers에 존재하는 숫자면 중복이니깐 true
-        return true;
+        return true; // 중복된 숫자가 있을 경우 true 반환
       }
       uniqueNumbers.add(number);
     }
-    return false;
+    return false; // 중복된 숫자가 없을 경우 false 반환
   }
 
-  // 겜 초기화
   void resetGame() {
     setState(() {
       answerNumbers.clear();
@@ -112,13 +107,13 @@ class _GameScreenState extends State<GameScreen> {
       message = '';
       createAnswerNumbers();
       showAnswer = false;
+      trueInput = true;
     });
   }
 
-  // 정답 보여주기
   void showAnswerNumbers() {
     setState(() {
-      showAnswer = true;
+      showAnswer = true; // 정답 보기 버튼을 누를 경우 정답 숫자 표시
     });
   }
 
@@ -133,17 +128,28 @@ class _GameScreenState extends State<GameScreen> {
         child: Column(
           children: [
             Text(
-              '중복되지 않는 숫자를 3개 입력해주세요. (1~9)',
+              '중복되지 않는 숫자를 3개 입력해주세요. (0~9)',
               style: TextStyle(fontSize: 18.0),
             ),
             SizedBox(height: 16.0),
-            TextField( // TextField를 이용해서 사용자의 입력을 받음. 입력 변경될때마다 onChanged
+            TextField(
               onChanged: (value) {
                 setState(() {
-                  if (value.length == 1 && input.contains(value)) {
-                    input = input.substring(0, input.length - 1);
+                  if (value.isEmpty) {
+                    input = '';
+                    message = '';
+                    trueInput = true; // 입력값이 공란 -> 유효
+                  } else if (int.tryParse(value) != null && value.length <= 3) {
+                    if (value.length == 1 && input.contains(value)) {
+                      input = input.substring(0, input.length - 1);
+                    } else {
+                      input = value;
+                    }
+                    message = '';
+                    trueInput = true; // 숫자 -> 유효
                   } else {
-                    input = value;
+                    message = '정수만 입력 가능합니다.';
+                    trueInput = false; // 숫자가 아님 -> 유효하지 않음
                   }
                 });
               },
@@ -154,14 +160,20 @@ class _GameScreenState extends State<GameScreen> {
               ),
             ),
             SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: checkAnswer,
-              child: Text('확인'),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: trueInput ? checkAnswer : null, // 입력값이 유효한 경우에만 버튼 활성화
+                child: Text('확인'),
+              ),
             ),
             SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: resetGame,
-              child: Text('재시작'),
+            Container(
+              width: double.infinity, // 가로 전체차지
+              child: ElevatedButton(
+                onPressed: resetGame,
+                child: Text('재시작'),
+              ),
             ),
             SizedBox(height: 16.0),
             if (showAnswer)
@@ -169,15 +181,21 @@ class _GameScreenState extends State<GameScreen> {
                 '정답: ${answerNumbers.join()}',
                 style: TextStyle(fontSize: 18.0),
               ),
-            ElevatedButton(
-              onPressed: showAnswerNumbers,
-              child: Text('정답 보기'),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: showAnswerNumbers,
+                child: Text('정답 보기'),
+              ),
             ),
             SizedBox(height: 16.0),
-            Text(
-              message,
-              style: TextStyle(fontSize: 18.0),
-              textAlign: TextAlign.center,
+            Container(
+              width: double.infinity,
+              child: Text(
+                message,
+                style: TextStyle(fontSize: 18.0),
+                textAlign: TextAlign.center,
+              ),
             ),
           ],
         ),
